1e021d4b45b90b1895ca99f07fd8389ed01b2dfd
ebramabd <abramabd151@gmail.com>
2025-10-04 17:23:52 +0300

    add google Analytics

--- resources/views/template/layout/header.blade.php
+++ resources/views/template/layout/header.blade.php
@@ -10,4 +10,13 @@
     <link href="{{ asset('template/home-img/icon-head.jpeg') }}" rel="icon" type="image/jpeg">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"/>
     <link href="{{ asset('template/css/templatemo-style.css') }}" rel="stylesheet">
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-YZ3GPYXK72"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-YZ3GPYXK72');
+    </script>
 </head>
